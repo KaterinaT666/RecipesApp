350f029df30540d64b7f2592924daf132c963123
79181 <katerina.tokan@yandex.ru>
2023-01-05 17:58:45 +0300

    Initial commit

---
+++ src/test/java/me/tokan/recipesapp/RecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tokan.recipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
